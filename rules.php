c6a6eaad9ffc0c919671b067885216999666484e
Nikola Posa <nikola.posa@arbor-education.com>
2017-04-07 10:20:01 +0200

    Removed 'no_unused_imports' from the ruleset as it doesn't work for commented parts of code.

--- rules.php
+++ rules.php
@@ -4,7 +4,6 @@ return [
     '@PSR2' => true,
     'array_syntax' => ['syntax' => 'short'],
     'object_operator_without_whitespace' => true,
-    'no_unused_imports' => true,
     'no_whitespace_in_blank_line' => true,
     'standardize_not_equals' => true,
     'no_extra_consecutive_blank_lines' => ['extra'],
